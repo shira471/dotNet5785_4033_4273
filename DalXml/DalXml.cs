7a996efb14183fc0960c43ea6183dbc601b1dd9d
shira alfasi <shalfasi@g.jct.ac.il>
2024-12-02 23:06:11 +0200

    stage3

--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -5,27 +5,27 @@ namespace Dal;
 
 public sealed class DalXml : Idal
 {
-    public Iassignment Assignment { get; } = new AssignmentImplementation();
+    public Iassignment assignment { get; } = new AssignmentImplementation();
 
-    public Iassignment assignment => throw new NotImplementedException();
+   // public Iassignment assignment => throw new NotImplementedException();
 
-    public Icall Call { get; } = new CallImplementation();
+    public Icall call { get; } = new CallImplementation();
 
-    public Icall call => throw new NotImplementedException();
+    //public Icall call => throw new NotImplementedException();
 
-    public Ivolunteer Volunteer { get; } = new VolunteerImplementation();
+    public Ivolunteer volunteer { get; } = new VolunteerImplementation();
 
-    public Ivolunteer volunteer => throw new NotImplementedException();
+    //public Ivolunteer volunteer => throw new NotImplementedException();
 
-    public Iconfig Config { get; } = new ConfigImplementation();
+    public Iconfig config { get; } = new ConfigImplementation();
 
-    public Iconfig config => throw new NotImplementedException();
+    //public Iconfig config => throw new NotImplementedException();
 
     public void ResetDB()
     {
-        Assignment.DeleteAll();
-        Call.DeleteAll();
-        Volunteer.DeleteAll();
-        Config.Reset();
+        assignment.DeleteAll();
+        call.DeleteAll();
+        volunteer.DeleteAll();
+        config.Reset();
     }
 }
