f32dcd46f8ddedcd84e40f95bd996556d4822b9a
shira alfasi <shalfasi@g.jct.ac.il>
2024-12-23 20:23:17 +0200

    stage5

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -8,7 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using BO;
+using BO.Enums;
 namespace PL
 {
     /// <summary>
@@ -16,9 +17,57 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
         public MainWindow()
         {
             InitializeComponent();
+            CurrentTime = DateTime.Now; // הגדרת זמן נוכחי כברירת מחדל
         }
+        // הגדרת תכונת תלות עבור CurrentTime
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentTimeProperty =
+            DependencyProperty.Register(
+                "CurrentTime",
+                typeof(DateTime),
+                typeof(MainWindow),
+                new PropertyMetadata(DateTime.Now)); // ערך ברירת מחדל
+        private void btnAddOneMinute_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceSystemClock(BO.Enums.TimeUnit.Minute);
+        }
+        private void btnAddOneHour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceSystemClock(BO.Enums.TimeUnit.Hour);
+        }
+        private void btnAddOneMonth_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceSystemClock(BO.Enums.TimeUnit.Month);
+        }
+        private void btnAddOneYear_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.AdvanceSystemClock(BO.Enums.TimeUnit.Year);
+        }
+        public int MaxYearRange
+        {
+            get { return (int)GetValue(MaxYearRangeProperty); }
+            set { SetValue(MaxYearRangeProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaxYearRangeProperty =
+            DependencyProperty.Register("MaxYearRange", typeof(int), typeof(MainWindow));
+        private void UpdateMaxRange_Click(object sender, RoutedEventArgs e)
+        {
+            // המרת השנים ל- TimeSpan (לדוגמה, כמה ימים יש בשנתיים)
+            TimeSpan timeSpan = TimeSpan.FromDays(MaxYearRange * 365);
+
+            // עדכון ערך משתנה התצורה דרך ה-BL
+            s_bl.Admin.SetRiskTimeSpan(timeSpan);
+        }
+
     }
 }
