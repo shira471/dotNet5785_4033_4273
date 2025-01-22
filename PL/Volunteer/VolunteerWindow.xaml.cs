73c89d7ffc39902b4dfa24654e8642add1ffcbdf
shira alfasi <shalfasi@g.jct.ac.il>
2025-01-22 18:59:40 +0200

    stage6

--- PL/Volunteer/VolunteerWindow.xaml.cs
+++ PL/Volunteer/VolunteerWindow.xaml.cs
@@ -136,9 +136,7 @@ namespace PL.Volunteer
             }
         }
 
-<<<<<<< HEAD
-  
-=======
+
    
         private void SelectCall_Click(object sender, RoutedEventArgs e)
         {
@@ -152,7 +150,7 @@ namespace PL.Volunteer
             myHistoryWindow.ShowDialog();
             LoadCallDetails();
         }
->>>>>>> 5a55c24a522e27eae870fc98980ccd408df9ccc7
+
 
         private void CancellationCall_Click(object sender, RoutedEventArgs e)
         {
